f49b7bfcf3e8b5452c529b4349f071b2830bff1a
hoang dzung <dungmin97@gmail.com>
2017-12-04 12:40:37 +0700

    update

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ def arg_parse():
 def downloadImage(longtitude, latitude, zoom_scale, key):
     try:
         print("Downloading...")
-        DIR = "{}_{}".format(longtitude, latitude)
+        DIR = os.path.join("static","{}_{}".format(longtitude, latitude))
         if not os.path.exists(DIR):
             os.mkdir(DIR)
-        os.chdir(DIR)
-        satellite_file="satellite.png"
-        mask_file="mask.png"
+        # os.chdir(DIR)
+        satellite_file=os.path.join(DIR,"satellite.png")
+        mask_file=os.path.join(DIR,"mask.png")
         urllib.urlretrieve(SATELLITE_DOWNLOAD_URL.format(longtitude,latitude, zoom_scale, key), satellite_file)
         urllib.urlretrieve(MASK_DOWNLOAD_URL.format(longtitude,latitude, zoom_scale, key), mask_file)
     except:
@@ -34,20 +34,24 @@ def downloadImage(longtitude, latitude, zoom_scale, key):
         return None
     else:
         print("Successful!")
-        return satellite_file, mask_file
+        return satellite_file, mask_file, DIR
 
-def toBinarySaliency(image):
+def toBinarySaliency(image, mask):
     imgsize = image.shape
     img_width  = imgsize[1]
     img_height = imgsize[0]
     sm = pySaliencyMap.pySaliencyMap(img_width, img_height)
     binarized_map = sm.SMGetBinarizedSM(cv2.medianBlur(image, 15))
-    cv2.imwrite("satellite_2b_bin_saliency.jpg", binarized_map)
+    mask=mask[:-50,:]
+    mask[mask>0]=255
+    bool_mask=mask.astype('bool')
+    binarized_map[bool_mask]=0
     return binarized_map
+
 def brightness(rgb):
     return rgb[0]*0.299 + rgb[1]*0.587 + rgb[2]*0.114
 
-def toBinaryKmean(image):
+def toBinaryKmean(image, mask):
     # Kmean
     Z = image.reshape((-1,3))
     Z = np.float32(Z)
@@ -65,27 +69,30 @@ def toBinaryKmean(image):
         center[0]=[0,0,0]
     img = center[label.flatten()]
     kmean = img.reshape((image.shape))
-    cv2.imwrite("satellite_1_kmean.jpg", kmean)
     # Binary
     thresh=cv2.cvtColor(kmean, cv2.COLOR_BGR2GRAY)
     # Noise filter
-    kernel = np.ones((5,5),np.float32)/25
+    kernel = np.ones((7,7),np.float32)/49
     filted = cv2.filter2D(thresh,-1,kernel)
     filted[filted>=127]=255
     filted[filted<127]=0
 
     # Dilation
-    kernel = np.ones((20,20),np.uint8)
+    kernel = np.ones((10,10),np.uint8)
     dilation = cv2.dilate(filted,kernel,iterations = 1)
-    cv2.imwrite("satellite_2a_bin_kmean.jpg", dilation)
+
+    mask=mask[:-50,:]
+    mask[mask>0]=255
+    bool_mask=mask.astype('bool')
+    dilation[bool_mask]=0
+
     return dilation
 
-def toBinaryCombine(image):
-    binary_saliency = toBinarySaliency(image)
-    binary_kmean = toBinaryKmean(image)
+def toBinaryCombine(image, mask):
+    binary_saliency = toBinarySaliency(image, mask)
+    binary_kmean = toBinaryKmean(image, mask)
 
-    binary_combine = cv2.bitwise_or(binary_saliency, binary_kmean)
-    cv2.imwrite("satellite_2c_bin_combine.jpg", binary_combine)
+    binary_combine = cv2.bitwise_and(binary_saliency, binary_kmean)
     return binary_combine
 
 def extractWater(satellite, mask):
@@ -100,22 +107,22 @@ def extractWater(satellite, mask):
 
     bool_mask = mask.astype('bool')
     satellite[bool_mask] = mean
-    cv2.imwrite("satellite_0_water_remove.jpg", satellite)
+    # cv2.imwrite("satellite_0_water_remove.jpg", satellite)
     return satellite, origin_image
 
-def detectContour(binary_image, origin_image, center, args):
-    log = args.longtitude
-    lat = args.latitude
-    zoom =args.zoom
+def detectContour(binary_image, origin_image, center, DIR, color):
+    # log = args.longtitude
+    # lat = args.latitude
+    # zoom =args.zoom
 
     _, contours,_ = cv2.findContours(binary_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     img = origin_image.copy()
-    count=0
+    count=len([i for i in os.listdir(DIR) if i[0].isdigit()])
     for cnt in contours:
         try:
             area = cv2.contourArea(cnt)
             (X,Y),(ma,Ma),angle = cv2.fitEllipse(cnt)
-            if area>500:
+            if area>200:
                 x,y,W,H = cv2.boundingRect(cnt)
                 rect = cv2.minAreaRect(cnt)
                 box = cv2.boxPoints(rect)
@@ -124,33 +131,35 @@ def detectContour(binary_image, origin_image, center, args):
                 w,h,_ = crop.shape
 
                 if (max(w,h)*1.0/min(w,h) > 2):
-                    img = cv2.drawContours(img,[box],-1,(0,255,0),2)
+                    img = cv2.drawContours(img,[box],-1,color,2)
                     
-                    dist_x = x+ W/2 -center[0]
-                    dist_y = y+ H/2 -center[1]
+                    # dist_x = x+ W/2 -center[0]
+                    # dist_y = y+ H/2 -center[1]
 
-                    center_latitude = lat+dist_x/ math.pow(2, zoom+1)
-                    center_longtitude = log-dist_y/math.pow(2,zoom+1)
+                    # center_latitude = lat+dist_x/ math.pow(2, zoom+1)
+                    # center_longtitude = log-dist_y/math.pow(2,zoom+1)
 
-                    cv2.putText(img,"({},{})".format(center_longtitude,center_latitude),(x,y+H/2), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255),2)
-                    img = cv2.circle(img,(x+W/2,y+H/2), 5, (0,0,255), -1)
+                    # cv2.putText(img,"({},{})".format(center_longtitude,center_latitude),(x,y+H/2), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255),2)
+                    # img = cv2.circle(img,(x+W/2,y+H/2), 5, (0,0,255), -1)
                     
-                    dist_x = x -center[0]
-                    dist_y = y -center[1]
+                    # dist_x = x -center[0]
+                    # dist_y = y -center[1]
 
-                    corner_latitude = lat+dist_x/ math.pow(2, zoom+1)
-                    corner_longtitude = log-dist_y/math.pow(2,zoom+1)
+                    # corner_latitude = lat+dist_x/ math.pow(2, zoom+1)
+                    # corner_longtitude = log-dist_y/math.pow(2,zoom+1)
 
-                    length = 2*vincenty((center_longtitude,center_latitude), (corner_longtitude, corner_latitude)).meters             
-                    cv2.putText(img,"length:{}".format(length),(x,y+H), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255),2)
+                    # length = 2*vincenty((center_longtitude,center_latitude), (corner_longtitude, corner_latitude)).meters             
+                    # cv2.putText(img,"length:{}".format(length),(x,y+H), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255),2)
                     
                     name = "{}.png".format(count)
+                    name = os.path.join(DIR, name)
                     cv2.imwrite(name, crop)
-                    print(name+" saved")
+                    # print(name+" saved")
                     count+=1
         except Exception as e:
-            print(e)
-    cv2.imwrite("satellite_3_result.png",img)
+            # print(e)
+            pass
+    return img
 
 def crop_minAreaRect(img, rect):
     # rotate img
@@ -192,15 +201,36 @@ def main():
     print("Binarizing image...\n")
     binary_img = np.zeros(extracted_water.shape[:2])
     if args.mode == 1:
-        binary_img = toBinaryKmean(extracted_water)
+        binary_img = toBinaryKmean(extracted_water, mask)
     elif args.mode == 2:
-        binary_img = toBinarySaliency(extracted_water)
+        binary_img = toBinarySaliency(extracted_water, mask)
     elif args.mode == 3:
-        binary_img = toBinaryCombine(extracted_water)
+        binary_img = toBinaryCombine(extracted_water, mask)
     binary_img = binary_img.astype(np.uint8)
     
     print("Detecting object...\n")
-    detectContour(binary_img, origin_image, center, args)
+    img = detectContour(binary_img, origin_image, center, args)
+
+def process(longtitude, latitude):
+
+    save_files = downloadImage(longtitude, latitude, 18, KEY)
+    satellite = cv2.imread(save_files[0])
+    center = (satellite.shape[0]/2,satellite.shape[1]/2)
+    mask = cv2.imread(save_files[1],0)
+    extracted_water, origin_image = extractWater(satellite, mask)
+    # os.remove(save_files[0])
+    # os.remove(save_files[1])
+
+    # binary_img = toBinaryKmean(extracted_water, mask)
+    # binary_img = binary_img.astype(np.uint8)
+
+    # origin_image = detectContour(binary_img, origin_image, center, save_files[2], (255,0,0))
+
+    binary_img = toBinaryCombine(extracted_water, mask)
+    binary_img = binary_img.astype(np.uint8)
+
+    origin_image = detectContour(binary_img, origin_image, center, save_files[2], (0,255,0))
+    cv2.imwrite(os.path.join(save_files[2], "result.png"),origin_image)
 
 if __name__ == '__main__':
     main()
